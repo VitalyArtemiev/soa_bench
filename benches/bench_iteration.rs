24a9c8e7ea4e4499b4eca93988c451acee9c4d6a
Виталий Артемьев <vitaly.artemiev@yandex.ru>
2021-11-11 03:59:31 +0300

    iterators are 2-3 times faster when not using all wields, and maybe slightly faster when using all fields (no bounds check?)

--- benches/bench_iteration.rs
+++ benches/bench_iteration.rs
@@ -1,6 +1,7 @@
 #![feature(test)]
 extern crate test;
 
+use test::black_box;
 use soa_bench::types::*;
 use test::Bencher;
 
@@ -21,13 +22,20 @@ fn bench_iter(bench: &mut Bencher) {
     }
 
     bench.iter(||{
+        let mut c= 0;
         for mut s in soa_vec.iter_mut() {
+            *s.color = Vec3i {x: c % 255, y: c % 255, z: c % 255};
             s.vel.x += 1;
             s.vel.y += -1;
+            //*s.name = s.vel.y.to_string();
 
             *s.pos += *s.vel;
+            c +=1;
+            *s.flags += s.pos.y as usize;
         }
-    })
+    });
+
+    black_box(soa_vec);
 }
 
 #[bench]
@@ -44,12 +52,28 @@ fn bench_for(bench: &mut Bencher) {
         s.vel.y += -1;
     }
 
-    bench.iter(||{
+    /*bench.iter(||{
         for i in 0..soa_vec.len() {
             soa_vec.vel[i].x += 1;
             soa_vec.vel[i].y += -1;
 
             soa_vec.pos[i] += soa_vec.vel[i];
         }
-    })
+    });*/
+
+    bench.iter(||{
+        let mut c= 0;
+        for i in 0..soa_vec.len() {
+            soa_vec.color[i] = Vec3i {x: c % 255, y: c % 255, z: c % 255};
+            soa_vec.vel[i].x += 1;
+            soa_vec.vel[i].y += -1;
+            //soa_vec.name[i] = soa_vec.vel[i].y.to_string();
+
+            soa_vec.pos[i] += soa_vec.vel[i];
+            c +=1;
+            soa_vec.flags[i] += soa_vec.pos[i].y as usize;
+        }
+    });
+
+    black_box(soa_vec);
 }
